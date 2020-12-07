9adb501933bfae7e5617b6fa93e1d6af7dc119fd
ToniCaimari <acaimari@cifpfbmoll.eu>
2020-12-07 22:37:40 +0100

    fix(crawler): :bug: content_.local_to_blogal/add test_*
    changed the variable content from local to global and added some tests
    
    content not accessible for functions

--- scraper/test/test_get_page_url.py
+++ scraper/test/test_get_page_url.py
@@ -1,9 +1,10 @@
-from src.crawler.py import get_page
+from src.crawler import get_page
 import pytest
 
 
 def test_existe_url():
     assert True
+
 
 def test_no_existe_url():
     assert False, "schooled!"
