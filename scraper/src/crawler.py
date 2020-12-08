038e5f6a630dee23c2cc22e7f500dff42b7ea50a
ToniCaimari <acaimari@cifpfbmoll.eu>
2020-12-08 05:54:59 +0100

    fix(crawler): :ambulance: filter not working!

--- scraper/src/crawler.py
+++ scraper/src/crawler.py
@@ -60,16 +60,24 @@ def crawl_web(page):
     return crawled
 
 
-def scrapper(page):
+def scrapper(page):  # page==link original
+    # crida al crawler i agafa la llista de links // page==link original
     to_scrap = crawl_web(page)
-    scrap_target = []
-    while to_scrap:
-        page = to_scrap.pop()
+    scrap_target = []  # datos que interesan
+    while to_scrap:  # 'mentres hi hagi links a to_scrap...'
+        page = to_scrap.pop()  # agafa s'ultim link de to_scrap (desapareix de to_scrap)//cambiam variable page. page == to_scrap.pop() (ultim link de la llista "to_scrap")
+        # converteix es link de page en un document que se pot llegir//page==ultim link de la llista "to_scrap"
         target = get_page(page)
-        if target.find('id="text-align-') != -1:
-            scrap_target.append(target)
+        initial_mark = target.find('id="text-align-')
+        if initial_mark != -1:
+            scrap_target.append(page)
         else:
             continue
+        # start_data = target.find(':', initial_mark)
+        # end_data = target.find('<', start_data)
+        # data = target[start_data + 1:end_data]
+        # scrap_target.append(data)
+        # initial_mark = start_data+1
 
 
 if __name__ == "__main__":
